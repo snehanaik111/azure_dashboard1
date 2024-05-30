af01da6a90f3a4f94bf3ecd3cb5dfb2fd50cc3c3
snehanaik111 <snehanaik1117@gmail.com>
2024-05-30 12:36:04 +0530

    Update app.py modbus change

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ def receive_level_sensor_data():
                 return jsonify({'status': 'failure', 'message': 'Request content type is not JSON'}), 400
             
             request_data = request.get_json()
-            modbus_test_data = request_data.get('modbus_TEST', '{}')
+            modbus_test_data = request_data.get('modbus_TEST')
             
           
             try:
